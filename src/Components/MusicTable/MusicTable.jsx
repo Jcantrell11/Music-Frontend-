c67c2731b238d1f1a2225b3338d2a9ad3997dc85
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-04-19 23:20:27 -0400

    Added form component for search button

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,28 +1,33 @@
 
 const MusicTable = (props) => {
     return (
-        <table>
-            <thead>
-                <th>Title</th>
-                <th>Artist</th>
-                <th>Album</th>
-                <th>Release Date</th>
-                <th>Genre</th>
-            </thead>
-            <tbody>
-                {props.parentSongs.map((setSongs, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{setSongs.title}</td>
-                            <td>{setSongs.artist}</td>
-                            <td>{setSongs.album}</td>
-                            <td>{setSongs.release_date}</td>
-                            <td>{setSongs.genre}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <><form>
+            <div>
+                <input type="text"></input>
+            </div>
+            <button type='submit'>Search</button>
+        </form><table>
+                <thead>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </thead>
+                <tbody>
+                    {props.parentSongs.map((setSongs, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{setSongs.title}</td>
+                                <td>{setSongs.artist}</td>
+                                <td>{setSongs.album}</td>
+                                <td>{setSongs.release_date}</td>
+                                <td>{setSongs.genre}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table></>
     )
 }
 
