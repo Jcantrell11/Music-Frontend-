50ee25b00eba24bb150a214370667d844f82d2d5
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-05-07 11:21:49 -0400

    Updated SearchBar + added new background image

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
 
 
-const SearchBar = ({searchLibrary}) => {
-    
-    const [query, setQuery] = useState('')
-    console.log(query)
-   
+function SearchBar({ searchLibrary }) {
+
+    const [query, setQuery] = useState('');
+    console.log(query);
+
     const handleSubmit = (event) => {
         event.preventDefault();
         searchLibrary(query);
-    }
-    
+    };
+
 
     const handleSearchChange = (event) => {
         setQuery(event.target.value);
-    }; 
-    
+    };
 
-       
-    
-    return ( 
+
+
+
+    return (
 
         <div>
             <form className="search" onSubmit={handleSubmit}>
-                <input 
-                type="text"
-                id="search" 
-                value={query}
-                onChange={handleSearchChange}>
+                <input
+                    type="text"
+                    id="search"
+                    value={query}
+                    onChange={handleSearchChange}>
                 </input>
-                <button value={query} onChange={handleSearchChange} type='search'>Search</button>
+                <button className="searchButton" value={query} onChange={handleSearchChange} type='search' style={{'margin': '0.5em'}}>Search</button>
             </form>
         </div>
-    
 
 
 
 
 
 
-     );
+
+    );
 }
  
 export default SearchBar;
